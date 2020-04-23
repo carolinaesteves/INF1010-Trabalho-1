0d043c7910291c8eb339c1cd663e0b643868c16f
anna verçosa <30809195+vercosa@users.noreply.github.com>
2020-04-23 18:18:04 -0300

    Arquivos da Ana

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "arvore_expressao.h"
+#include <stdio.h>
+
+
+int main(void){
+
+    char postfix[80] = ""; // o postfix
+
+    //cria arvore de espressão
+    avrExp* a = arv_criavazia();
+
+    //passa postfix para arvore de expressao
+    a = pfx_arvexp(postfix,a);
+    //imprime arvore de expressao de forma aninhada
+    arv_imprime(a);
+
+    //calcula a expressao
+    printf("%d", calculaExpressao(a));
+    return 0;
+}
